3470cb6f8bbc9d39fb2b98cedd487ddbe8c25c8e
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-25 10:01:00 -0500

    added error and validation checks

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,9 +1,42 @@
 from django.db import models
+from datetime import datetime, date
+
+class Show_Manager(models.Manager):
+    def basic_validator(self, post_data):
+        errors = {}
+
+        if len(post_data["title"]) < 1:
+            errors["title"] = "Movie title must be at least 2 characters"
+        
+        if len(post_data["network"]) < 1:
+            errors["network"] = "Movie network must be at least 2 characters"
+        
+        if len(post_data["description"]) > 0:
+            if len(post_data["description"]) < 10:
+                errors["description"] = "Movie description must be at least 10 characters"
+        
+        if len(post_data["release_date"]) < 1:
+            errors["release_date"] = "Please select or enter a  release date"
+        if post_data["release_date"].isalpha() == True:
+            errors["release_date"] = "Release Date must be a valid date"
+        if len(post_data["release_date"]) > 0:
+            date_entered = datetime.strptime(post_data["release_date"], "%m/%d/%Y")
+            date_today = date.today().strftime("%m/%d/%y")
+            d3 = datetime.strptime(date_today, "%m/%d/%y")
+            if date_entered > d3:
+                errors["release_date"] = "Release Date must be in the past"
+
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     release_date = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = Show_Manager()
+
+    class Meta:
+        ordering = ['title']
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -1,7 +1,9 @@
-from django.http import request
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.http import request
 from .models import Show
 from datetime import datetime, date
+
 
 
 def shows(request):
@@ -12,34 +14,40 @@ def shows(request):
 
 def add_show(request):
     if request.method == "POST":
-        date_string = request.POST["release_date"]
-        format_date_string = datetime.strptime(date_string, "%m/%d/%Y")
-        print(type(format_date_string))
-        print(format_date_string)
+        errors = Show.objects.basic_validator(request.POST)
+
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/new')
+
         Show.objects.create(
             title = request.POST["title"],
             network = request.POST["network"],
             description = request.POST["description"],
-            release_date = datetime.strptime(date_string, "%m/%d/%Y")
+            release_date = datetime.strptime(request.POST["release_date"], "%m/%d/%Y")
         )        
         return redirect('/')
     else:
         return render(request, "add-show.html")
 
 def edit_show(request, show_id):
-    show_to_edit = Show.objects.get(id=show_id)
-    today = date.today()
-
     if request.method == "POST":
-        date_string = request.POST["release_date"]
+        errors = Show.objects.basic_validator(request.POST)
+
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/shows/{show_id}/edit')
+
+        show_to_edit = Show.objects.get(id=show_id)
         # UPDATE
         show_to_edit.title = request.POST["title"]
         show_to_edit.network = request.POST["network"]
         show_to_edit.description = request.POST["description"]
-        show_to_edit.release_date = datetime.strptime(date_string, "%m/%d/%Y")
-        show_to_edit.updated_at = today
+        show_to_edit.release_date = datetime.strptime(request.POST["release_date"], "%m/%d/%Y")
+        show_to_edit.updated_at = date.today()
         show_to_edit.save()
-        # return redirect('/')
         return redirect(f'/shows/{show_id}')
     else:
         context = {
